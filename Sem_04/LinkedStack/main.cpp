cf628ac2e57eeb415e3742e11ba683115bce9f29
MariaGrozdeva <64608453+MariaGrozdeva@users.noreply.github.com>
2021-11-17 16:22:52 +0200

    Add files via upload

---
+++ Sem_04/LinkedStack/main.cpp
@@ -0,0 +1,22 @@
+#include "LinkedStack.hpp"
+
+int main()
+{
+	LinkedStack<int> st1;
+	st1.push(22);
+	st1.push(2);
+	st1.push(3);
+	st1.push(33);
+
+	st1.pop();
+	st1.pop();
+	st1.pop();
+
+	LinkedStack<int> st2(st1);
+	st2.push(23);
+	st2.push(24);
+	st2.push(25);
+
+	st1.pop();
+	cout << "st1 is empty: " << boolalpha << (st1.empty()) << endl;
+}
