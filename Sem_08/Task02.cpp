f1b9984886a3eb8dea10ad8d6525beaf2ffddc77
MariaGrozdeva <64608453+MariaGrozdeva@users.noreply.github.com>
2021-11-25 18:02:08 +0200

    Add files via upload

---
+++ Sem_08/Task02.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+template <typename T>
+struct Node
+{
+	T data;
+
+	Node* left;
+	Node* right;
+
+	Node(const T& data) : data(data), left(nullptr), right(nullptr)
+	{}
+};
+
+int sum(Node<int>* root)
+{
+	if (!root)
+		return 0;
+
+	return root->data + sum(root->left) + sum(root->right);
+}
+
+int main()
+{
+	Node<int>* root = new Node<int>(2);
+	Node<int>* n1 = new Node<int>(7);
+	Node<int>* n2 = new Node<int>(5);
+	Node<int>* n3 = new Node<int>(2);
+	Node<int>* n4 = new Node<int>(6);
+	Node<int>* n5 = new Node<int>(9);
+	Node<int>* n6 = new Node<int>(5);
+	Node<int>* n7 = new Node<int>(11);
+	Node<int>* n8 = new Node<int>(4);
+
+	root->left = n1;
+	root->right = n2;
+	n1->left = n3;
+	n1->right = n4;
+	n2->right = n5;
+	n4->left = n6;
+	n4->right = n7;
+	n5->left = n8;
+
+	cout << sum(root);
+}
---
+++ Sem_08/Task03.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+template <typename T>
+struct Node
+{
+	T data;
+
+	Node* left;
+	Node* right;
+
+	Node(const T& data) : data(data), left(nullptr), right(nullptr)
+	{}
+};
+
+template <typename T>
+int getMaxElement(Node<T>* root)
+{
+	if (!root)
+		return INT_MIN;
+
+	return max(root->data, max(getMaxElement(root->left), getMaxElement(root->right)));
+}
+
+int main()
+{
+	Node<int>* root = new Node<int>(2);
+	Node<int>* n1 = new Node<int>(7);
+	Node<int>* n2 = new Node<int>(5);
+	Node<int>* n3 = new Node<int>(2);
+	Node<int>* n4 = new Node<int>(6);
+	Node<int>* n5 = new Node<int>(9);
+	Node<int>* n6 = new Node<int>(5);
+	Node<int>* n7 = new Node<int>(11);
+	Node<int>* n8 = new Node<int>(4);
+
+	root->left = n1;
+	root->right = n2;
+	n1->left = n3;
+	n1->right = n4;
+	n2->right = n5;
+	n4->left = n6;
+	n4->right = n7;
+	n5->left = n8;
+
+	cout << getMaxElement(root);
+}
