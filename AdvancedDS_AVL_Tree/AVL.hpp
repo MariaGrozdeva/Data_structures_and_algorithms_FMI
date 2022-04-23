ac222e524704ca0017100d7b286b99c5a9492c46
Maria Grozdeva <64608453+MariaGrozdeva@users.noreply.github.com>
2022-04-23 18:44:36 +0300

    Add files via upload

---
+++ AdvancedDS_AVL_Tree/AVL.hpp
@@ -0,0 +1,388 @@
+#pragma once
+
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+
+template <typename T>
+class AVL
+{
+private:
+	struct Node
+	{
+	public:
+		T* data;
+		size_t height;
+
+		Node* left;
+		Node* right;
+		Node* parent;
+
+		Node(const T& data) : data(new T(data)), height(1), left(nullptr), right(nullptr), parent(nullptr)
+		{}
+		Node(const Node& other)
+		{
+			copyFrom(other);
+		}
+		Node& operator=(const Node& other)
+		{
+			if (this != &other)
+			{
+				free();
+				copyFrom(other);
+			}
+
+			return *this;
+		}
+		~Node()
+		{
+			free();
+		}
+
+	private:
+		void copyFrom(const Node& other)
+		{
+			data = new T(*other.data);
+			height = other.height;
+		}
+		void free()
+		{
+			delete data;
+		}
+
+
+	public:
+		void swap(Node& other) // Swapping ONLY pointers.
+		{
+			std::swap(data, other.data);
+		}
+
+		size_t getHeight() const // The height of an empty node is 0.
+		{
+			if (!this)
+				return 0;
+
+			return height;
+		}
+		int getBalanceFactor() const // height(right) - height(left). If we want the tree to be AVL, then the balance factor should be in {-1,0,1}.
+		{
+			return right->getHeight() - left->getHeight();
+		}
+
+		void rotateLeft()
+		{
+			if (!this || !this->right)
+				return;
+
+			swap(*right); // Swapping the data pointers of this and this->right.
+
+			Node* originalRight = right;
+			right = originalRight->right;
+			originalRight->right = originalRight->left;
+			originalRight->left = left;
+			left = originalRight;
+
+			// Updating the heights of the affected (because of the rotation) nodes.
+			left->height = std::max(left->left->getHeight(), left->right->getHeight()) + 1;
+			height = std::max(left->getHeight(), right->getHeight()) + 1;
+
+			// Updating the parents of the affected (because of the rotation) nodes.
+			updateParents();
+		}
+		void rotateRight()
+		{
+			if (!this || !this->left)
+				return;
+
+			swap(*left); // Swapping the data pointers of this and this->left.
+
+			Node* originalLeft = left;
+			left = originalLeft->left;
+			originalLeft->left = originalLeft->right;
+			originalLeft->right = right;
+			right = originalLeft;
+
+			// Updating the heights of the affected (because of the rotation) nodes.
+			right->height = std::max(right->left->getHeight(), right->right->getHeight()) + 1;
+			height = std::max(left->getHeight(), right->getHeight()) + 1;
+
+			// Updating the parents of the affected (because of the rotation) nodes.
+			updateParents();
+		}
+
+	private:
+		void updateParents()
+		{
+			if (left)
+			{
+				left->parent = this;
+				if (left->left)
+					left->left->parent = left;
+				if (left->right)
+					left->right->parent = left;
+			}
+			if (right)
+			{
+				right->parent = this;
+				if (right->left)
+					right->left->parent = right;
+				if (right->right)
+					right->right->parent = right;
+			}
+		}
+	};
+
+	void copyFromRec(Node*& root, Node* otherRoot);
+	void freeRec(Node* root);
+
+	void copyFrom(const AVL<T>& other);
+	void free();
+
+public:
+	AVL() = default;
+	AVL(const AVL<T> & other);
+	AVL<T>& operator=(const AVL<T> & other);
+	~AVL();
+
+
+private:
+	void leftLeftCase(Node* current);
+	void rightRightCase(Node* current);
+	void leftRightCase(Node* current);
+	void rightLeftCase(Node* current);
+
+	void update(Node* current);
+
+	void findMax(Node* root, Node*& maxNode) const;
+	void eraseRec(Node*& root, const T& el);
+
+	bool containsRec(Node* root, const T& el, Node*& parent) const;
+
+public:
+	Node* root = nullptr;
+
+	void insert(const T& el);
+	void erase(const T& el);
+
+	bool contains(const T& el) const;
+	bool empty() const;
+};
+
+template <typename T>
+void AVL<T>::leftLeftCase(Node* current)
+{
+	current->rotateRight();
+}
+template <typename T>
+void AVL<T>::rightRightCase(Node* current)
+{
+	current->rotateLeft();
+}
+template <typename T>
+void AVL<T>::leftRightCase(Node* current)
+{
+	current->left->rotateLeft();
+	current->rotateRight();
+}
+template <typename T>
+void AVL<T>::rightLeftCase(Node* current)
+{
+	current->right->rotateRight();
+	current->rotateLeft();
+}
+
+template <typename T>
+void AVL<T>::update(Node* current)
+{
+	if (!current)
+		return;
+
+	current->height = 1 + std::max(current->left->getHeight(), current->right->getHeight());
+	int balanceFactor = current->getBalanceFactor();
+
+	// If the current node becomes unbalanced, then there are 4 cases:
+	if (balanceFactor == -2 && current->left->getBalanceFactor() < 0)
+		leftLeftCase(current);
+
+	else if (balanceFactor == 2 && current->right->getBalanceFactor() > 0)
+		rightRightCase(current);
+
+	else if (balanceFactor == -2 && current->left->getBalanceFactor() > 0)
+		leftRightCase(current);
+
+	else if (balanceFactor == 2 && current->right->getBalanceFactor() < 0)
+		rightLeftCase(current);
+
+	update(current->parent); // Keep going up the tree and updating heights and balance factors.
+}
+
+template <typename T>
+void AVL<T>::insert(const T& el)
+{
+	if (!root)
+	{
+		root = new Node(el);
+		return;
+	}
+
+	Node* parent = nullptr;
+	bool found = containsRec(root, el, parent);
+	if (found)
+		return;
+
+	Node* newNode = new Node(el);
+	if (*(parent->data) > el)
+		parent->left = newNode;
+	else
+		parent->right = newNode;
+
+	newNode->parent = parent;
+
+	update(parent); // Maintain the tree to be AVL. In this update we make <= 2 rotations.
+}
+
+template <typename T>
+void AVL<T>::findMax(Node* root, Node*& maxNode) const
+{
+	Node* iter = root;
+	while (iter)
+	{
+		maxNode = iter;
+		iter = iter->right;
+	}
+}
+template <typename T>
+void AVL<T>::eraseRec(Node*& root, const T& el)
+{
+	if (!root)
+		return;
+
+	Node* parent = nullptr;
+	if (!containsRec(root, el, parent))
+		return;
+
+	Node*& toErase = parent == nullptr ? root :
+		el < *parent->data ? parent->left : parent->right;
+
+	// remove leaf
+	if (!toErase->left && !toErase->right)
+	{
+		Node* toEraseParent = toErase->parent;
+		delete toErase;
+		toErase = nullptr;
+
+		update(toEraseParent); // Maintain the tree to be AVL. In this update we can make up to O(h) rotations. Therefore deletion of an element in AVL
+		                       // is slower than insertion. If we have a lot of deletions (and insertions), Red–black trees are preferred over AVL trees.
+	}
+
+	// remove node with only left child (this child is certainly a leaf because the tree is AVL (otherwise, the tree wouldn't be balanced).
+	else if (toErase->left && !toErase->right)
+	{
+		toErase->swap(*toErase->left);
+		eraseRec(toErase->left, el);
+	}
+	// remove node with only right child (this child is certainly a leaf because the tree is AVL (otherwise, the tree wouldn't be balanced).
+	else if (!toErase->left && toErase->right)
+	{
+		toErase->swap(*toErase->right);
+		eraseRec(toErase->right, el);
+	}
+
+	// remove node with left and right children
+	else
+	{
+		Node* maxNode;
+		findMax(toErase->left, maxNode);
+		toErase->swap(*maxNode);
+		eraseRec(toErase->left, el);
+	}
+}
+template <typename T>
+void AVL<T>::erase(const T& el)
+{
+	eraseRec(root, el);
+}
+
+template <typename T>
+bool AVL<T>::containsRec(Node* root, const T& el, Node*& parent) const
+{
+	if (!root)
+		return false;
+	if (*(root->data) == el)
+		return true;
+
+	parent = root;
+	if (*(root->data) > el)
+		return containsRec(root->left, el, parent);
+	containsRec(root->right, el, parent);
+}
+template <typename T>
+bool AVL<T>::contains(const T& el) const
+{
+	return containsRec(root, el);
+}
+
+template <typename T>
+bool AVL<T>::empty() const
+{
+	return root == nullptr;
+}
+
+template <typename T>
+AVL<T>::AVL(const AVL<T>& other)
+{
+	copyFrom(other);
+}
+template <typename T>
+AVL<T>& AVL<T>::operator=(const AVL<T>& other)
+{
+	if (this != &other)
+	{
+		free();
+		copyFrom(other);
+	}
+
+	return *this;
+}
+template <typename T>
+AVL<T>::~AVL()
+{
+	free();
+}
+
+template <typename T>
+void AVL<T>::copyFrom(const AVL<T>& other)
+{
+	copyFromRec(root, other.root);
+}
+template <typename T>
+void AVL<T>::free()
+{
+	freeRec(root);
+}
+
+template <typename T>
+void AVL<T>::copyFromRec(Node*& root, Node* otherRoot)
+{
+	if (!otherRoot)
+	{
+		root = nullptr;
+		return;
+	}
+
+	root = new Node(*otherRoot);
+
+	copyFromRec(root->left, otherRoot->left);
+	copyFromRec(root->right, otherRoot->right);
+}
+template <typename T>
+void AVL<T>::freeRec(Node* root)
+{
+	if (!root)
+		return;
+
+	freeRec(root->left);
+	freeRec(root->right);
+
+	delete root;
+}
---
+++ AdvancedDS_DSW_algorithm/DSW.cpp
@@ -0,0 +1,168 @@
+#include <iostream>
+#include <algorithm>
+
+template <typename T>
+struct Node
+{
+	T* data;
+	Node<T>* left;
+	Node<T>* right;
+
+	Node(const T& data) : data(new T(data)), left(nullptr), right(nullptr)
+	{}
+};
+
+template <typename T>
+void free(Node<T>* root)
+{
+	if (!root)
+		return;
+
+	free(root->left);
+	free(root->right);
+
+	delete root->data;
+	delete root;
+}
+
+size_t closestLowerPowerOfTwo(size_t n)
+{
+	size_t result = 0;
+	for (size_t i = n; i >= 1; i--)
+	{
+		if ((i & (i - 1)) == 0)
+		{
+			result = i;
+			break;
+		}
+	}
+	return result;
+}
+
+template <typename T>
+size_t height(Node<T>* root)
+{
+	if (!root)
+		return 0;
+
+	return 1 + std::max(height(root->left), height(root->right));
+}
+
+template <typename T>
+void rotateLeft(Node<T>*& root)
+{
+	if (!root || !root->right)
+		return;
+
+	std::swap(root->data, root->right->data);
+	Node<T>* originalRight = root->right;
+	root->right = originalRight->right;
+	originalRight->right = originalRight->left;
+	originalRight->left = root->left;
+	root->left = originalRight;
+}
+template <typename T>
+void rotateRight(Node<T>*& root)
+{
+	if (!root || !root->left)
+		return;
+
+	std::swap(root->data, root->left->data);
+	Node<T>* originalLeft = root->left;
+	root->left = originalLeft->left;
+	originalLeft->left = originalLeft->right;
+	originalLeft->right = root->right;
+	root->right = originalLeft;
+}
+
+// Balance the tree using the Day-Stout-Warren algorithm.
+
+template <typename T>
+bool DSW(Node<T>*& root)
+{
+	// 1. Make the tree completely unbalanced (straight) to the right by doing right rotations on all left children.
+
+	Node<T>* currentNode = root; // Start at the root.
+	size_t nodesCount = 0; // Keep track of the number of nodes in this tree.
+
+	while (currentNode) // Go from the root to the bottom right node.
+	{
+		while (currentNode->left) // While we still see a left child.
+			rotateRight(currentNode); // Get rid of the left child.
+		if (nodesCount == 0)
+			root = currentNode; // After we've done all rotations on the root, update the original one. 
+		currentNode = currentNode->right; // Update our current node to it's right child.
+		nodesCount++;
+	}
+
+	// If the tree height isn't equal to the number of nodes in the tree, this step failed.
+	if (nodesCount != height(root))
+		return false;
+
+	// 2. Do left rotations and balance the tree (currently - backbone(vine)).
+
+	size_t leavesOnBottomLevel = nodesCount - closestLowerPowerOfTwo(nodesCount) + 1; // The number of nodes on the bottom level.
+	currentNode = root;
+
+	for (size_t i = 0; i < leavesOnBottomLevel; i++) // For the nodes on the bottom level:
+	{
+		if (i == 0) // Rotate the root.
+		{
+			rotateLeft(currentNode);
+			root = currentNode;
+		}
+		else // If not the root, rotate the right of the current node.
+			rotateLeft(currentNode->right);
+	}
+
+	size_t rotationsCount = nodesCount;
+	while (rotationsCount > 1) // The number of rotations are reduced twice each time because as we go up the tree, the number of nodes on a certain level are 50% of all nodes (on this level + the upper ones).
+	{
+		rotationsCount /= 2;
+		currentNode = root;
+
+		for (size_t i = 0; i < rotationsCount - 1; i++) // For the length of the vine:
+		{
+			if (i == 0) // Rotate the root.
+			{
+				rotateLeft(currentNode);
+				root = currentNode;
+			}
+			else
+			{
+				currentNode = currentNode->right; // Update the current node.
+				rotateLeft(currentNode); // Rotate the current node.
+			}
+		}
+	}
+
+	// The tree should be balanced. The height of it (measured in number of edges) is expected to be log(# nodes).
+	if (ceil(log(nodesCount)) != height(root) - 1)
+		return false;
+
+	return true;
+}
+
+int main()
+{
+	Node<int>* root = new Node<int>(10);
+	Node<int>* n1 = new Node<int>(5);
+	Node<int>* n2 = new Node<int>(20);
+	Node<int>* n3 = new Node<int>(7);
+	Node<int>* n4 = new Node<int>(15);
+	Node<int>* n5 = new Node<int>(30);
+	Node<int>* n6 = new Node<int>(25);
+	Node<int>* n7 = new Node<int>(40);
+	Node<int>* n8 = new Node<int>(23);
+	root->left = n1;
+	root->right = n2;
+	n1->right = n3;
+	n2->left = n4;
+	n2->right = n5;
+	n5->left = n6;
+	n5->right = n7;
+	n6->left = n8;
+
+	std::cout << DSW(root);
+	free(root);
+}
