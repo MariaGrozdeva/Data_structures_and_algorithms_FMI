5afa0c926a786688c76af2e1180bdfce37aeee1b
Maria Grozdeva <64608453+MariaGrozdeva@users.noreply.github.com>
2023-03-14 16:37:12 +0200

    Update main.cpp

--- Sem_04/SinglyLinkedList/main.cpp
+++ Sem_04/SinglyLinkedList/main.cpp
@@ -2,36 +2,33 @@
 
 int main()
 {
-	SinglyLinkedList<int> l;
+	SinglyLinkedList<int> l1 = { 1, 2, 3, 4, 5 };
 	SinglyLinkedList<int> l2;
 
-	l.push_back(1);
-	l.push_back(2);
-	l.push_back(3);
-	l.push_back(4);
-	l.push_back(5);
+	l2 = std::move(l1); // move operator= called
+	SinglyLinkedList<int> l3(std::move(l2)); // move copy constr called
+	SinglyLinkedList<int> l4(l3); // copy constr called
 
-	l.push_front(0);
-	l.push_front(-1);
-	l.push_front(-2);
+	l3.push_back(10);
+	l3.push_back(11);
+	l3.push_back(12);
+	l3.push_back(13);
+	l3.push_back(14);
+	cout << l3 << endl;
 
-	l.print();
-	cout << endl;
+	l3.push_front(0);
+	l3.push_front(-1);
+	l3.push_front(-2);
+	cout << l3 << endl;
 
-	l.pop_front();
-	l.pop_front();
-	l.pop_front();
+	l3.pop_front();
+	l3.pop_front();
+	l3.pop_front();
+	cout << l3 << endl;
 
-	l.print();
-	cout << endl;
+	cout << l3.front() << endl;
+	cout << l3.back() << endl;
 
-	cout << l.front() << endl;
-	cout << l.back() << endl;
-
-	l2.push_back(6);
-	l2.push_back(7);
-	l2.push_back(8);
-
-	SinglyLinkedList<int> res = concat(l, l2);
-	res.print();
+	SinglyLinkedList<int> res = concat(l3, l4);
+	std::cout << res;
 }
