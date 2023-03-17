254aa9355847f210acf952b0ce3c65959b78d5a0
Maria Grozdeva <64608453+MariaGrozdeva@users.noreply.github.com>
2023-03-17 15:53:16 +0200

    sorting method added to sll

--- Sem_04/SinglyLinkedList/main.cpp
+++ Sem_04/SinglyLinkedList/main.cpp
@@ -1,12 +1,9 @@
-#include "SinglyLinkedList.hpp"
-
-int main()
-{
-	SinglyLinkedList<int> l1 = { 1, 2, 3, 4, 5 };
+SinglyLinkedList<int> l1 = { 1, 2, 3, 4, 5 };
 	SinglyLinkedList<int> l2;
 
 	l2 = std::move(l1); // move operator= called
 	SinglyLinkedList<int> l3(std::move(l2)); // move copy constr called
+	std::cout << "Initial list:" << std::endl << l3 << std::endl << std::endl;
 	SinglyLinkedList<int> l4(l3); // copy constr called
 
 	l3.push_back(10);
@@ -14,21 +11,24 @@ int main()
 	l3.push_back(12);
 	l3.push_back(13);
 	l3.push_back(14);
-	cout << l3 << endl;
+	std::cout << "List after calling push_back with 10, 11, 12, 13 and 14:" << std::endl << l3 << std::endl << std::endl;
 
 	l3.push_front(0);
 	l3.push_front(-1);
 	l3.push_front(-2);
-	cout << l3 << endl;
+	std::cout << "List after calling push_front with 0, -1 and -2:" << std::endl << l3 << std::endl << std::endl;
 
 	l3.pop_front();
 	l3.pop_front();
 	l3.pop_front();
-	cout << l3 << endl;
+	std::cout << "List after calling pop_front three times:" << std::endl << l3 << std::endl << std::endl;
 
-	cout << l3.front() << endl;
-	cout << l3.back() << endl;
+	std::cout << "Calling front returns: " << l3.front() << std::endl << std::endl;
+	std::cout << "Calling back returns: " << l3.back() << std::endl << std::endl;
 
 	SinglyLinkedList<int> res = concat(l3, l4);
-	std::cout << res;
+	std::cout << "New list after concatenating the old one with \"1 -> 2 -> 3 -> 4 -> 5\":" << std::endl << res << std::endl << std::endl;
+
+	res.sort([](int a, int b) { return a <= b; });
+	std::cout << "List after sorting in increasing order:" << std::endl << res << std::endl << std::endl;
 }
