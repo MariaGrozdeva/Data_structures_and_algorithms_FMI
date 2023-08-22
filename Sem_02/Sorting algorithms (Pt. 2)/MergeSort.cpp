b0df78774019a515b649972376e8c42da2995adf
Maria Grozdeva <64608453+MariaGrozdeva@users.noreply.github.com>
2023-08-22 17:54:39 +0300

    Update MergeSort.cpp

--- Sem_02/Sorting algorithms (Pt. 2)/MergeSort.cpp
+++ Sem_02/Sorting algorithms (Pt. 2)/MergeSort.cpp
@@ -5,59 +5,59 @@ using namespace std;
 template <typename T>
 void Merge(T* arr1, size_t len1, T* arr2, size_t len2, T* result)
 {
-    unsigned arr1Iter = 0;
-    unsigned arr2Iter = 0;
-    unsigned resultIter = 0;
+    	unsigned arr1Iter = 0;
+    	unsigned arr2Iter = 0;
+    	unsigned resultIter = 0;
     
-    while (arr1Iter < len1 && arr2Iter < len2)
-    {
-        if (arr1[arr1Iter] <= arr2[arr2Iter])
-            result[resultIter++] = arr1[arr1Iter++];
-        else
-            result[resultIter++] = arr2[arr2Iter++];    
-    }
+    	while (arr1Iter < len1 && arr2Iter < len2)
+    	{
+        	if (arr1[arr1Iter] <= arr2[arr2Iter])
+            		result[resultIter++] = arr1[arr1Iter++];
+        	else
+            		result[resultIter++] = arr2[arr2Iter++];    
+    	}
     
-    while (arr1Iter < len1)
-        result[resultIter++] = arr1[arr1Itresult[resultIter++] = arr2[arr2Iter++];er++];
+    	while (arr1Iter < len1)
+        	result[resultIter++] = arr1[arr1Itresult[resultIter++] = arr2[arr2Iter++];er++];
     
-    while (arr2Iter < len2)
-        result[resultIter++] = arr2[arr2Iter++];   
+    	while (arr2Iter < len2)
+        	result[resultIter++] = arr2[arr2Iter++];   
 }
 
 template <typename T>
 void MergeSortStep(T* arr, size_t len, T* bufferArray)
 {
-    if (len <= 1)
-        return;
+    	if (len <= 1)
+        	return;
     
-    unsigned midIndex = len / 2;
+    	unsigned midIndex = len / 2;
     
-    MergeSortStep<T>(arr, midIndex, bufferArray);
-    MergeSortStep<T>(arr + midIndex, len - midIndex, bufferArray);
+    	MergeSortStep<T>(arr, midIndex, bufferArray);
+    	MergeSortStep<T>(arr + midIndex, len - midIndex, bufferArray);
     
-    Merge<T>(arr, midIndex, arr + midIndex, len - midIndex, bufferArray);
+    	Merge<T>(arr, midIndex, arr + midIndex, len - midIndex, bufferArray);
     
-    for (size_t i = 0; i < len; i++)
-        arr[i] = bufferArray[i];
+    	for (size_t i = 0; i < len; i++)
+        	arr[i] = bufferArray[i];
 }
 
 // 6.1. Merge sort (Complexity - 0(nlog(n)))
 template <typename T>
 void MergeSort(T* arr, size_t len)
 {
-    if (!arr || len == 0)
+    	if (!arr || len == 0)
 		return;
 		
-    T* bufferArray = new T[len];
+    	T* bufferArray = new T[len];
     
-    MergeSortStep<T>(arr, len, bufferArray);
+    	MergeSortStep<T>(arr, len, bufferArray);
     
-    delete[] bufferArray;
+    	delete[] bufferArray;
 }
 
 int main()
 {
-    int arr[8] = { 170, 45, 75, 90, 802, 24, 2, 66 };
+    	int arr[8] = { 170, 45, 75, 90, 802, 24, 2, 66 };
 	MergeSort(arr, 8);
 
 	for (int i = 0; i < 8; i++)
