35a4d4f546d4297a7b7f9c642f2ec6cf946f99b3
Maria Grozdeva <64608453+MariaGrozdeva@users.noreply.github.com>
2026-03-24 18:15:48 +0200

    Add files via upload

---
+++ Sem_10/specificPairOfIndices.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <set>
+
+
+bool containsNearbyAlmostDuplicate(std::vector<int>& nums, int indexDiff,
+                                   int valueDiff)
+{
+    std::multiset<long long> window;
+
+    for (int i = 0; i < (int)nums.size(); ++i)
+    {
+        long long x = nums[i];
+
+        auto it = window.lower_bound(x - (long long)valueDiff);
+        if (it != window.end() && *it <= x + (long long)valueDiff)
+        {
+            return true;
+        }
+
+        window.insert(x);
+
+        if (i >= indexDiff)
+        {
+            window.erase(window.find((long long)nums[i - indexDiff]));
+        }
+    }
+
+    return false;
+}
+
+int main()
+{
+    std::vector<int> nums1 = {1, 2, 3, 1};
+    int indexDiff1 = 3;
+    int valueDiff1 = 0;
+    std::cout << containsNearbyAlmostDuplicate(nums1, indexDiff1, valueDiff1) << '\n';
+
+    std::vector<int> nums2 = {1, 5, 9, 1, 5, 9};
+    int indexDiff2 = 2;
+    int valueDiff2 = 3;
+    std::cout << containsNearbyAlmostDuplicate(nums2, indexDiff2, valueDiff2) << '\n';
+
+    return 0;
+}
